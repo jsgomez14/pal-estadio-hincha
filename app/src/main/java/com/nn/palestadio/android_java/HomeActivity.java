85e1d7cd48f9231a417d5bf81a59ea684f02dbc0
dalazaca <diezucchet@gmail.com>
2018-03-23 00:09:49 -0500

    Floating Button added with navegation to Ticket Scan Activity.

--- app/src/main/java/com/nn/palestadio/android_java/HomeActivity.java
+++ app/src/main/java/com/nn/palestadio/android_java/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.nn.palestadio.android_java;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button floatingButton = (Button) findViewById(R.id.floatingButton);
+        floatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, ScanTicketActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
