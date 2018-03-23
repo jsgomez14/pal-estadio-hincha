dfe8fafad4068b39085d5ed11542a965f060de80
dalazaca <diezucchet@gmail.com>
2018-03-23 00:08:10 -0500

    Activity where you'll scan the ticket added

---
+++ app/src/main/java/com/nn/palestadio/android_java/ScanTicketActivity.java
@@ -0,0 +1,13 @@
+package com.nn.palestadio.android_java;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScanTicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_ticket);
+    }
+}
