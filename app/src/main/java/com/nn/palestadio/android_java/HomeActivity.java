20744f051a13d822c21603e3f524adb21d0a0d10
Juan Sebastian Gomez Celis <js.gomez14@uniandes.edu.co>
2018-03-28 12:44:22 -0500

    Intent from HomeACtivity to ScanTicketActivity added (when clicking FloatingActionButton)

--- app/src/main/java/com/nn/palestadio/android_java/HomeActivity.java
+++ app/src/main/java/com/nn/palestadio/android_java/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.nn.palestadio.android_java;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,13 +14,13 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        /*Button floatingButton = (Button) findViewById(R.id.floatingButton);
+        FloatingActionButton floatingButton = (FloatingActionButton) findViewById(R.id.fab);
         floatingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HomeActivity.this, ScanTicketActivity.class);
                 startActivity(intent);
             }
-        });*/
+        });
     }
 }
