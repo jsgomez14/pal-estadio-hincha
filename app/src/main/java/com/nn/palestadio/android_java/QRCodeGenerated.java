688e1aa052da3f447dd73cd18bebda60232d4ebb
Juan Sebastian Gomez Celis <js.gomez14@uniandes.edu.co>
2018-03-30 15:09:11 -0500

    Generate QR functionality implemented.

---
+++ app/src/main/java/com/nn/palestadio/android_java/QRCodeGenerated.java
@@ -0,0 +1,84 @@
+package com.nn.palestadio.android_java;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+public class QRCodeGenerated extends AppCompatActivity {
+    public final static int QRcodeWidth = 500 ;
+    private Bitmap bitmap;
+    private ImageView QRviz;
+    private Button botonVolver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode_generated);
+
+        QRviz = (ImageView) findViewById(R.id.QRViz);
+        String bc = getIntent().getStringExtra("EXTRA_BARCODE_SCANNED");
+        botonVolver = (Button) findViewById(R.id.boton_volver);
+
+        botonVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(QRCodeGenerated.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        try {
+            bitmap = TextToImageEncode(bc);
+
+            QRviz.setImageBitmap(bitmap);
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Bitmap TextToImageEncode(String Value) throws WriterException
+    {
+        BitMatrix bitMatrix;
+        try
+        {
+            bitMatrix = new MultiFormatWriter().encode(
+                    Value,
+                    BarcodeFormat.DATA_MATRIX.QR_CODE,
+                    QRcodeWidth, QRcodeWidth, null
+            );
+
+        } catch (IllegalArgumentException Illegalargumentexception) {
+
+            return null;
+        }
+        int bitMatrixWidth = bitMatrix.getWidth();
+
+        int bitMatrixHeight = bitMatrix.getHeight();
+
+        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+        for (int y = 0; y < bitMatrixHeight; y++) {
+            int offset = y * bitMatrixWidth;
+
+            for (int x = 0; x < bitMatrixWidth; x++) {
+
+                pixels[offset + x] = bitMatrix.get(x, y) ?
+                        getResources().getColor(R.color.QRCodeBlackColor):getResources().getColor(R.color.QRCodeWhiteColor);
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+
+        bitmap.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        return bitmap;
+    }
+}
